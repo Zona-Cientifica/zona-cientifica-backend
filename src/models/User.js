c73fe1e882f774f2613cc1d7b3ffc264c6ae7759
Guilherme Costa <gui-costaquino@hotmail.com>
2023-11-23 12:24:07 -0300

    Adição de array de favoritos ao model User

--- src/models/User.js
+++ src/models/User.js
@@ -4,10 +4,11 @@ const User = mongoose.model('User', {
   name: String,
   apelido: String,
   email: String,
-  telefone: Number,
+  telefone: String,
   password: String,
   passwordResetToken: String,
   purchaseHistoric: Array,
+  participatingList: Array,
   favoriteList: Array
 })
 
