3549b292eb0a45cd3ec19cf7872597ea153a6800
leomendes18 <leomendesmonteiro1@gmail.com>
2023-12-13 09:13:12 -0300

    implementando função de remover o favorito

--- src/controller/users/favoriteList.js
+++ src/controller/users/favoriteList.js
@@ -1,25 +1,50 @@
-const User = require("../../models/User")
+const User = require("../../models/User");
 
+async function addFavorite(req, res) {
+  User.findOne({ email: req.body.email }).then((user) => {
+    user.favoriteList.push(req.body.eventName);
 
-  async function addFavorite(req, res){
-    User.findOne({email: req.body.email}).then((user)=>{
-        user.favoriteList.push(req.body.eventName);
+    user
+      .save()
+      .then(() => {
+        res
+          .status(200)
+          .json({ message: "Evento adicionado à lista de favoritos" });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: "Ocorreu um erro: " + err });
+      });
+  });
+}
 
-        user.save().then(()=>{
-            res.status(200).json({message: 'Evento adicionado à lista de favoritos'})
-        }).catch((err)=>{
-            res.status(400).json({message: 'Ocorreu um erro: '+err});
-        })
+async function getFavoriteList(req, res) {
+  User.findOne({ email: req.body.email })
+    .lean()
+    .then((user) => {
+      res.status(200).json({ favoriteList: user.favoriteList });
     })
-  }
+    .catch((err) => {
+      res.status(400).json({ message: "Erro na busca: " + err });
+    });
+}
 
+async function deleteFavorite(req, res) {
+  User.findOne({ email: req.body.email }).then((user) => {
+    let index = user.favoriteList.indexOf(req.body.eventName);
+    if (index > -1) {
+      user.favoriteList.splice(index, 1);
+    }
+    user
+      .save()
+      .then(() => {
+        res
+          .status(200)
+          .json({ message: "Evento removido à lista de favoritos" });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: "Ocorreu um erro: " + err });
+      });
+  });
+}
 
-  async function getFavoriteList(req, res){
-    User.findOne({email: req.body.email}).lean().then((user)=>{
-        res.status(200).json({favoriteList: user.favoriteList})
-    }).catch((err)=>{
-        res.status(400).json({message: 'Erro na busca: '+err})
-    })
-  }   
-
-  module.exports = {addFavorite, getFavoriteList}
+module.exports = { addFavorite, getFavoriteList, deleteFavorite };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const multer = require("multer");
-const multerConfig = require("../config/multer")
-const Post = require("../models/post")
+const multerConfig = require("../config/multer");
+const Post = require("../models/post");
 const register = require("../controller/users/register");
 const login = require("../controller/users/login");
 const forgot_password = require("../controller/users/forgot_password");
 const confirmEdit = require("../controller/users/edit_perfil");
-const purchaseHistoric = require('../controller/users/purchaseHistoric');
-const cardRegister = require('../controller/cardRegister');
-const favoriteList = require('../controller/users/favoriteList');
-const findEvent = require('../../src/controller/findEvent');
+const purchaseHistoric = require("../controller/users/purchaseHistoric");
+const cardRegister = require("../controller/cardRegister");
+const favoriteList = require("../controller/users/favoriteList");
+const findEvent = require("../../src/controller/findEvent");
 const registerEvent = require("../controller/registerEvent");
 
 router.post("/login", login);
@@ -18,31 +18,32 @@ router.post("/register", register);
 
 router.post("/forgot_password", forgot_password);
 
-router.post('/perfil', confirmEdit);
+router.post("/perfil", confirmEdit);
 
-router.get('/events', findEvent.findEvents);
+router.get("/events", findEvent.findEvents);
 
-router.post('/event', registerEvent);
+router.post("/event", registerEvent);
 
-router.post('/buyEvent', purchaseHistoric.buyEvent);
+router.post("/buyEvent", purchaseHistoric.buyEvent);
 
-router.post('/getPurchaseHistoric', purchaseHistoric.getPurchaseHistoric);
+router.post("/getPurchaseHistoric", purchaseHistoric.getPurchaseHistoric);
 
-router.post('/cardregister', cardRegister);
+router.post("/cardregister", cardRegister);
 
-router.post('/addFavorite', favoriteList.addFavorite);
+router.post("/addFavorite", favoriteList.addFavorite);
 
-router.post('/getFavoriteList', favoriteList.getFavoriteList);
+router.post("/getFavoriteList", favoriteList.getFavoriteList);
 
-router.post('/posts', multer(multerConfig).single('file'), async (req, res) =>{
-    const post = await Post.create({
-        name: req.file.originalname,
-        size: req.file.size,
-        key: req.file.filename,
-        url: '',
-    })
-    return res.json();
-})
+router.post("/deleteFavorite", favoriteList.deleteFavorite);
 
+router.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const post = await Post.create({
+    name: req.file.originalname,
+    size: req.file.size,
+    key: req.file.filename,
+    url: "",
+  });
+  return res.json();
+});
 
 module.exports = router;
