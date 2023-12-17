dd8c75194eb873babfb45368649d3d56f641f165
Guilherme Costa <gui-costaquino@hotmail.com>
2023-12-17 14:17:15 -0300

    Recebendo imagem para edição de perfil

--- src/controller/users/edit_perfil.js
+++ src/controller/users/edit_perfil.js
@@ -1,12 +1,14 @@
-const User = require("../../models/User")
+const User = require("../../models/User");
 
-  
 async function confirmEdit(req, res) {
-  const {name, username, phone, email} = req.body;
-
-  const user = await User.updateOne({email: email}, {$set: {name, userName:username, phone}});
+  const { name, username, phone, email } = req.body;
+  const picture = req.file?.filename;
+  const user = await User.updateOne(
+    { email: email },
+    { $set: { name, userName: username, phone, picture } }
+  );
 
   res.send("Usuário atualizado");
 }
 
-  module.exports =  confirmEdit;
+module.exports = confirmEdit;
